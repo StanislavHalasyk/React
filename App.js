049f58760c962f95f8451f3cee46f0048140c7cd
Stanislav Halasyk <galasik_stanislav@icloud.com>
2023-10-23 21:58:50 +0300

    hw

--- App.js
+++ App.js
@@ -1,28 +1,16 @@
-import { useState, createContext } from 'react';
 import './App.css';
-import Tasks from './components/Tasks';
+import Counter from './Counter';
 
-export const Context = createContext(null);
 
 function App () {    
-  const [tasks, setTasks] = useState([]);
-  const [name,setName] = useState('');
 
-  const handleNewTask = () => {
-  const items = [...tasks, name];
-  setTasks(items);
-  console.log(items)
-  }
+  return (        
 
-  return (   
-
-    <Context.Provider value={tasks}>
       <div>
-        <input type='text' placeholder='Add your name' value={name} onChange={(e) => setName(e.target.value)}></input>
-        <button onClick={handleNewTask}>Add name to list</button> 
-        <Tasks />   
-      </div>
-    </Context.Provider>
+          <h2>Reduce counter:</h2>
+          <Counter />    
+      </div>  
+         
   )
 }
 
